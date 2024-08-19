85d894373362fbf4f1bf46029583b712df060683
Carlos Takata <carlos.takata@zazmic.com>
2024-08-19 15:14:00 -0300

    fix

--- mongo-seed/init.js
+++ mongo-seed/init.js
@@ -1,6 +1,5 @@
 db = db.getSiblingDB('ffc_database'); // Replace 'your_database' with your database name
 db.createCollection('orders'); // Replace 'your_collection' with your collection name
 db.your_collection.insertMany([
-  { store: 'test', name: 'Alice', date: new Date('2024-08-19T20:00:00Z'), value: 35, prevHash: 0, Hash: 0 },
-  { store: 'test', name: 'Bob', date: new Date('2024-08-19T20:01:00Z'), value: 45, prevHash: 0, Hash: 0 },
+  { store: 'test', name: 'Alice', date: new Date('2024-08-19T20:00:00Z'), value: 35, prevHash: 0, Hash: 0 }
 ]);
